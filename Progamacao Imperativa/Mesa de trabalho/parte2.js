4d46adb4692074f14142695fd7ecc55732d86a28
michel <michelvieiradonascimento20@gmail.com>
2022-06-14 19:46:04 -0400

    att

--- Progamacao Imperativa/Mesa de trabalho/parte2.js
+++ Progamacao Imperativa/Mesa de trabalho/parte2.js
@@ -1,56 +1,6 @@
 
 // parte 2
 
-const espectador = [
-    {
-        idade: 18,
-        opiniao: 2
-    },
-    {
-        idade: 28,
-        opiniao: 1
-    },
-    {
-        idade: 15,  opiniao: 3
-    },
-    {
-        idade: 22,  opiniao: 1
-    },
-    {
-        idade: 82,  opiniao: 2
-    },
-    {
-        idade: 31,  opiniao: 2
-    },
-    {
-        idade: 19,  opiniao: 3
-    },
-    {
-        idade: 25,  opiniao: 3
-    },
-    {
-        idade: 41,  opiniao: 1
-    },
-    {
-        idade: 17,  opiniao: 3
-    },
-    {
-        idade: 30,  opiniao: 2
-    },
-    {
-        idade: 36,  opiniao: 1
-    },
-    {
-        idade: 13,  opiniao: 1
-    },
-    {
-        idade: 45,  opiniao: 3
-    },
-    {
-        idade: 25,  opiniao: 2
-    }
-];
-
 var idades = [18, 20, 25, 17, 40, 35, 39, 58, 65, 28, 21, 41, 14, 80, 12];
 var opinioes = [1, 2, 3, 2, 1, 2, 3, 2, 1, 2, 3, 2, 1, 2, 3];
 
